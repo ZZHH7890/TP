7776c025b8639eec04947d09826c890763b2409c
jokerzhang <joker@17track.cn>
2020-07-13 19:36:03 +0800

    修改测试用例功能

--- testPlatform/tpTest/urls.py
+++ testPlatform/tpTest/urls.py
@@ -2,7 +2,7 @@
 @Author: joker.zhang
 @Date: 2020-06-23 14:30:15
 @LastEditors: joker.zhang
-@LastEditTime: 2020-07-12 23:11:13
+@LastEditTime: 2020-07-13 14:29:48
 @Description: For Automation
 '''
 
@@ -14,5 +14,7 @@ urlpatterns = [
     path('<int:test_case_pk>', views.test_case_detail, name='test_case_detail'),
     path('add_test_case/', views.add_form, name='add_form'),
     path('add_test_case/add_action/', views.add_action, name='add_action'),
+    path('test_case_list/update_form/', views.update_form, name='update_form'),
+    path('test_case_list/update_form/update_action/', views.update_action, name='update_action'),
     path('test_case_list/delete_action/', views.delete_action, name='delete_action'),
 ]
--- testPlatform/tpTest/views.py
+++ testPlatform/tpTest/views.py
@@ -2,10 +2,10 @@
 @Author: joker.zhang
 @Date: 2020-06-23 10:50:40
 @LastEditors: joker.zhang
-@LastEditTime: 2020-07-12 23:14:33
+@LastEditTime: 2020-07-13 16:20:26
 @Description: For Automation
 '''
-from django.shortcuts import render, get_object_or_404, HttpResponseRedirect, render_to_response
+from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
 from django.contrib.auth.models import User
 from django.core.paginator import Paginator
 from .models import TestCases
@@ -30,17 +30,12 @@ def add_action(request):
         re = request.POST["TC_remark"]
         TestCases.objects.create(TC_name=name, TC_set_up=su, TC_params=pa, TC_checks=cer,
                                  TC_next_step=ns, TC_project=pro, TC_version=ver, owner=oer, TC_remark=re)
-        test_case_all_list = TestCases.objects.all()
-        paginator = Paginator(test_case_all_list, 5)
-        page_num = request.GET.get('page', 1)
-        page_of_test_cases = paginator.get_page(page_num)
-        context = {}
-        context['page_of_test_cases'] = page_of_test_cases
-        return render(request, 'tpTest/test_case_list.html', context)
-    else:
-        context = {'welcome': '添加失败'}
-        return render_to_response('home.html', context)
-
+    test_case_all_list = TestCases.objects.all()
+    paginator = Paginator(test_case_all_list, 5)
+    page_num = request.GET.get('page', 1)
+    page_of_test_cases = paginator.get_page(page_num)
+    context = {}
+    context['page_of_test_cases'] = page_of_test_cases
 
 def delete_action(request):
     if request.method == "POST":
@@ -54,6 +49,38 @@ def delete_action(request):
     context = {}
     context['page_of_test_cases'] = page_of_test_cases
     return render(request, 'tpTest/test_case_list.html', context)
+
+
+def update_form(request):
+    if request.method == "POST":
+        test_case_pk = request.POST["test_case_id"]
+        test_case = TestCases.objects.get(id=test_case_pk)
+    context = {}
+    context['test_case'] = test_case
+    return render(request, 'tpTest/update_test_case.html', context)
+
+
+def update_action(request):
+    if request.method == "POST":
+        test_case_pk = request.POST["test_case_id"]
+        name = request.POST["TC_name"]
+        su = request.POST["TC_set_up"]
+        pa = request.POST["TC_params"]
+        cer = request.POST["TC_checks"]
+        ns = request.POST["TC_next_step"]
+        pro = request.POST["TC_project"]
+        ver = request.POST["TC_version"]
+        oer = User.objects.get(id=request.POST["owner"])
+        re = request.POST["TC_remark"]
+        TestCases.objects.filter(id=test_case_pk).update(TC_name=name, TC_set_up=su, TC_params=pa, TC_checks=cer,
+                                                         TC_next_step=ns, TC_project=pro, TC_version=ver, owner=oer, TC_remark=re)
+        test_case_all_list = TestCases.objects.all()
+        paginator = Paginator(test_case_all_list, 5)
+        page_num = request.GET.get('page', 1)
+        page_of_test_cases = paginator.get_page(page_num)
+        context = {}
+        context['page_of_test_cases'] = page_of_test_cases
+        return render(request, 'tpTest/test_case_list.html', context)
 
 
 def test_case_list(request):
